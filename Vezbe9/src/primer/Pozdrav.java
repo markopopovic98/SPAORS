efcc54b1feec7357ed899102a95a52eadc4ef6eb
markopopovic98 <markopoppriboj@gmail.com>
2018-01-12 17:09:20 +0100

    huhasd

--- Vezbe9/src/primer/Pozdrav.java
+++ Vezbe9/src/primer/Pozdrav.java
@@ -2,13 +2,13 @@ package primer;
 
 public class Pozdrav {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Zdravo!");
+
 		
-=======
+		
+
 		System.out.println("Zdravo! marko");
 		System.out.println("Zdravo, i tebi!");   
->>>>>>> refs/heads/marko-popovic
+
 	}
 }
 

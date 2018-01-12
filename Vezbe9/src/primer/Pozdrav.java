24de17662fb2fe271e5e31d92d299939915402a5
markopopovic98 <markopoppriboj@gmail.com>
2018-01-12 16:48:00 +0100

    "uzvracamoooo"

--- Vezbe9/src/primer/Pozdrav.java
+++ Vezbe9/src/primer/Pozdrav.java
@@ -3,6 +3,6 @@ package primer;
 public class Pozdrav {
 	public static void main(String[] args) {
 		System.out.println("Zdravo!");
-		System.out.println("Zdravo, i tebi!");
+		System.out.println("Zdravo, i tebi!");   
 	}
 }
